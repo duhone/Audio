98c76efab004def20d0f3c0c9c482e0d997349e1
duhone <duhone@outlook.com>
2020-07-27 08:07:15 -0700

    stub project

---
+++ build/build.cmake
@@ -0,0 +1,75 @@
+set(root "${CMAKE_CURRENT_LIST_DIR}/..")
+
+###############################################
+#library
+###############################################
+set(PUBLIC_HDRS
+    ${root}/inc/Audio/AudioEngine.h
+)
+
+set(SRCS
+    ${root}/src/AudioEngine.cpp
+)
+
+set(BUILD
+    ${root}/build/build.cmake
+)
+
+add_library(audio  
+	${PUBLIC_HDRS} 
+	${SRCS} 
+	${SRCS_SHADERS} 
+	${BUILD}
+)
+	
+settingsCR(audio)	
+usePCH(audio core)	
+
+target_include_directories(audio PUBLIC
+	"${root}/inc"
+)
+target_include_directories(audio PRIVATE
+	"${root}/src"
+)		
+
+target_link_libraries(audio PUBLIC
+    doctest
+    robinmap
+    core
+    function2
+)
+					
+###############################################
+#unit tests
+###############################################
+set(SRCS
+  ${root}/tests/main.cpp
+  ${root}/tests/AudioEngine.cpp
+)
+
+add_executable(audio_tests
+					${SRCS}
+)
+
+settingsCR(audio_tests)	
+usePCH(audio_tests core)
+					
+target_include_directories(audio_tests PRIVATE
+	"${root}/src"
+)	
+
+target_link_libraries(audio_tests 
+	doctest
+	fmt
+	core
+	platform
+	audio
+)
+
+add_dependencies(audio_tests AudioProcessor)
+
+add_custom_command(TARGET audio_tests POST_BUILD
+    COMMAND $<TARGET_FILE:AudioProcessor> -i ${root}/tests/data/BGM_Menu.wav -o $<TARGET_FILE_DIR:audio_tests>/BGM_Menu
+)
+
+set_property(TARGET audio_tests APPEND PROPERTY FOLDER tests)
---
+++ tests/main.cpp
@@ -0,0 +1,17 @@
+﻿#define DOCTEST_CONFIG_IMPLEMENT
+#include <3rdParty/doctest.h>
+
+int main(int argc, char** argv) {
+	doctest::Context context;
+
+	context.applyCommandLine(argc, argv);
+
+	int res;
+	{
+		res = context.run();
+	}
+
+	if(context.shouldExit()) return res;
+
+	return res;
+}
